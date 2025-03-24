54f6e3c7680a3e959cc7abcafaba529d55db7300
Mausam Giri <mausamgirimantracare@gmail.com>
2025-03-24 16:26:50 +0530

    heading added

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
       <div>
+        <h1>This is heading</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
